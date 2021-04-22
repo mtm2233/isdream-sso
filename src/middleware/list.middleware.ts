764ce23cd6432112be75d786f7f8d843960c8d23
MA <2541817610@qq.com>
2021-04-22 22:46:23 +0800

    style: 调整limit中间件

--- src/middleware/list.middleware.ts
+++ src/middleware/list.middleware.ts
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-04-13 23:38:29
- * @LastEditTime: 2021-04-19 12:48:44
+ * @LastEditTime: 2021-04-22 22:37:46
  * @LastEditors: mTm
  */
 import { Context } from 'koa'
@@ -11,6 +11,10 @@ const limit = async (ctx: Context, next: () => Promise<any>) => {
     const { page = 1, pageSize = 10 } = ctx.query;
     ctx.query.offset = `${(Number(page) - 1) * Number(pageSize)}`;
     ctx.query.size = `${pageSize}`;
+    ctx.meta = {
+        pageSize,
+        page,
+    }
     await next();
 }
 
