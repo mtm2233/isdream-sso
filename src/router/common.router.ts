4d77cb2bd428ab504aae35e4e6ff9c0a6c6d3836
MA <2541817610@qq.com>
2021-04-23 14:23:46 +0800

    :sparkles: common 添加中间件进行约束

--- src/router/common.router.ts
+++ src/router/common.router.ts
@@ -2,12 +2,14 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-04-21 23:28:56
- * @LastEditTime: 2021-04-22 22:59:22
+ * @LastEditTime: 2021-04-23 14:23:21
  * @LastEditors: mTm
  */
 import * as Router from 'koa-router'
 
 import commonController from '../controller/common.controller'
+
+import { verifyAuth, verifyPermission } from '../middleware/auth.middleware'
 
 import {
     createConfig,
@@ -19,14 +21,24 @@ import {
 
 const commonRouter = new Router({prefix: '/common'})
 
-commonRouter.post('/', commonController.create(createConfig))
+commonRouter.post('/', verifyAuth, commonController.create(createConfig))
 
-commonRouter.delete('/:articleId', commonController.remove(removeConfig))
+commonRouter.delete('/:articleId', verifyAuth, verifyPermission, commonController.remove(removeConfig))
 
 commonRouter.get('/', commonController.list(listConfig))
 
-commonRouter.patch('/:articleId', commonController.update(updateConfig))
+commonRouter.patch('/:articleId', verifyAuth, verifyPermission, commonController.update(updateConfig))
 
 commonRouter.get('/:articleId', commonController.detail(detailConfig))
+
+// commonRouter.post('/', commonController.create(createConfig))
+
+// commonRouter.delete('/:articleId', commonController.remove(removeConfig))
+
+// commonRouter.get('/', commonController.list(listConfig))
+
+// commonRouter.patch('/:articleId', commonController.update(updateConfig))
+
+// commonRouter.get('/:articleId', commonController.detail(detailConfig))
 
 export default commonRouter
