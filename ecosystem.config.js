302f7ba7dc4b260d812407e2423979982326d125
MA <2541817610@qq.com>
2021-05-02 22:15:42 +0800

    chore: pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: mTm
+ * @Date: 2021-05-02 22:14:02
+ * @LastEditTime: 2021-05-02 22:15:02
+ * @LastEditors: mTm
+ */
+module.exports = {
+  apps: [
+    {
+      name: "generalPlatformApi",
+      script: "./dist/main.js",
+      watch: true,
+    },
+  ],
+};
