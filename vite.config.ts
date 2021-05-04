a75f4856bc1ec00ba7019156af5aee8304694bca
MA <2541817610@qq.com>
2021-05-04 22:59:04 +0800

    chore: vite build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: mTm
  * @Date: 2021-05-01 22:19:41
- * @LastEditTime: 2021-05-03 15:16:19
+ * @LastEditTime: 2021-05-04 22:58:36
  * @LastEditors: mTm
  */
 import { defineConfig } from 'vite'
@@ -30,9 +30,6 @@ export default defineConfig({
     },
   },
   base: './', // 设置打包路径
-  build: {
-    outDir: './dist1'  
-  },
   server: {
     host: '0.0.0.0',
     port: 9001, // 设置服务启动端口号
